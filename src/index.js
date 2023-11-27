b931a3cd6f363a307255bfe3177aa9a678a0a6d2
ryabchikk <mixail-12-r@mail.ru>
2023-11-27 15:26:27 +0300

    release task

--- src/index.js
+++ src/index.js
@@ -3,26 +3,43 @@ import * as d3 from "d3";
 import * as Geo from './geo.json'
 
 document.addEventListener("DOMContentLoaded", setup)
-
+const map= new Map()
 function setup(){
     const spaceX = new SpaceX();
     spaceX.launches().then(data=>{
         const listContainer = document.getElementById("listContainer")
         renderLaunches(data, listContainer);
-        drawMap();
+        //drawMap();
     })
+    spaceX.launchpads().then(data=>{drawMap(data);})
 }
 function renderLaunches(launches, container){
     const list = document.createElement("ul");
     launches.forEach(launch=>{
         const item = document.createElement("li");
         item.innerHTML = launch.name;
+        item.id = launch.id;
+        item.onmouseover = selectRegion;
+        item.onmouseout = freeRegion;
         list.appendChild(item);
+        map.set(launch.id,launch.launchpad);
+        
     })
     container.replaceChildren(list);
 }
+function selectRegion(obj){
+    const id = map.get(obj.target.id);
+    const elem = document.getElementById(id);
+    elem.setAttribute("fill","red");
+}
 
-function drawMap(){
+function freeRegion(obj){
+    const id = map.get(obj.target.id);
+    const elem = document.getElementById(id);
+    elem.setAttribute("fill","black");
+}
+
+function drawMap(launchpads){
     const width = 640;
     const height = 480;
     const margin = {top: 20, right: 10, bottom: 40, left: 100};
@@ -45,8 +62,23 @@ function drawMap(){
         .attr("d", d3.geoPath()
             .projection(projection)
         )
-        .attr("fill", function (d) {
-            return colorScale(0);
-        })
         .style("opacity", .7)
+
+    const array = new Array();
+
+    launchpads.forEach(launchpad=>{
+        array.push({"type":"Feature","id":launchpad.id,"geometry":{"type":"Point","coordinates":[launchpad.longitude,launchpad.latitude]}})
+    })
+    
+    svg.append("g")
+        .selectAll("path")
+        .data(array)
+        .enter()
+        .append("path")
+        .attr("class", "point")
+        .attr("d", d3.geoPath()
+            .projection(projection)
+        )
+        .attr("id",(elem) => elem.id);
+    console.log(launchpads);
 }
